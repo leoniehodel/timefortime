798e2555d8429d63c72824dc52a2f1e5349f98bb
Leonie Hodel <leonie@hodel.net>
2017-09-11 09:58:02 +0200

    css changes

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -3,8 +3,8 @@ import './body.html';
 
 Template.body.helpers({
   offers: [
-    { text: 'Clean your House' },
-    { text: 'English lessons' },
-    { text: 'Fitness training' },
+    { title: 'Clean your House', description: ' I can clean' },
+    { title: 'English lessons', description: 'from a native speaker' },
+    { title: 'Fitness training', description: 'I\'ll make you strong' },
   ],
 });
